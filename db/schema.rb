6cecca0e4c76679709bcb45769d77ee9b0326a6f
Eric Kelly <heroiceric@gmail.com>
2014-06-08 21:54:02 -0400

    User can view a meetup's details

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140606200404) do
+ActiveRecord::Schema.define(version: 20140609013928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetups", force: true do |t|
+    t.string   "name",        null: false
+    t.text     "description", null: false
+    t.string   "location",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "provider",   null: false
